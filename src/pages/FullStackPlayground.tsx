011dc92ac16185bb7b3fbcec3043f4abaae79080
Siddhant saxena <66401356+mr-siddy@users.noreply.github.com>
2025-06-08 20:09:42 +0530

    Logo Update

--- src/pages/FullStackPlayground.tsx
+++ src/pages/FullStackPlayground.tsx
@@ -201,9 +201,11 @@ const FullStackPlayground = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center group">
-            <div className="w-10 h-10 bg-white rounded-2xl flex items-center justify-center mr-3">
+            {/* <div className="w-10 h-10 bg-white rounded-2xl flex items-center justify-center mr-3">
               <Code className="w-6 h-6 text-black" />
-            </div>
+            </div> */}
+            <img src="/logo_solo.png" alt="SimWorks Logo" className="h-10 w-auto mr-3" />
+
             <span className="text-2xl font-bold text-white">
               SimWorks
             </span>
